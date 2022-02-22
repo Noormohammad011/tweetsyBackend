17e88905e28875d9062e639755854fd71ab15f80
Noormohammad011 <noormohammad.net1503011@gmail.com>
2022-02-22 16:30:48 +0600

    'backend'

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient
+const ObjectId = require('mongodb').ObjectId
+
+//initialization
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+//monogdb Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@noyonecommerce.qnayd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+app.get('/', (req, res) => {
+  res.send('Running my CRUD Server')
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+async function run() {
+  try {
+    await client.connect()
+    const TaskCollection = client.db('TaskCollection')
+
+    app.get('/tasks', async (req, res) => {
+      const tasks = await TaskCollection.collection('tasks').find().toArray()
+      res.json(tasks)
+    })
+    app.post('/tasks', async (req, res) => {
+      const tasks = await TaskCollection.collection('tasks').insertOne(req.body)
+      res.json(tasks)
+    })
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir)
